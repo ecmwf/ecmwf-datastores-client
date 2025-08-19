3c545322106f6ae29f1de162cbf3d67f1636c702
malmans2 <malmans2@users.noreply.github.com>
2025-08-19 15:10:14 +0000

    Deploy preview for PR 26 🛫

---
+++ pr-preview/pr-26/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '1000.dev1+g00bdf0b89',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
